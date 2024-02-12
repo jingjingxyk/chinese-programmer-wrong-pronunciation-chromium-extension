9cb2b5193e256f121ff12f86b77531ca46c8c606
jingjingxyk <zonghengbaihe521@qq.com>
2024-02-12 20:05:56 +0800

    更新元素选择器

--- js/app/init.js
+++ js/app/init.js
@@ -3,7 +3,15 @@ import box from "./box.js";
 let init = () => {
   let URLObj = new URL(location.href);
   console.log(URLObj);
-  if (document.querySelector("#readme table tbody")) {
+  if (
+    document.querySelector(
+      "#repo-content-pjax-container article.markdown-body table",
+    )
+  ) {
+    let table_element = document.querySelector(
+      "#repo-content-pjax-container article.markdown-body table",
+    );
+
     //载入自定义组件样式
     box.styleConfig();
     //载入自定义组件
@@ -11,66 +19,64 @@ let init = () => {
 
     let audio_player = new Audio();
     audio_player.setAttribute("autoplay", "true");
-    document
-      .querySelector("#readme table tbody")
-      .addEventListener("click", (event) => {
-        //console.log(event.target)
-        // console.log(event.target.nodeType)
-        // console.log(event.target.nodeName);
-        let parentElement = event.target.parentElement;
-        if (parentElement && parentElement.nodeName === "TR") {
-          if (parentElement.firstElementChild === event.target) {
-            //使用搜索引擎查询发音
-            box.goToSearchPronounce(event.target.innerText);
-          }
+    table_element.addEventListener("click", (event) => {
+      //console.log(event.target)
+      // console.log(event.target.nodeType)
+      // console.log(event.target.nodeName);
+      let parentElement = event.target.parentElement;
+      console.log(parentElement.nodeName);
+      if (parentElement && parentElement.nodeName === "TR") {
+        if (parentElement.firstElementChild === event.target) {
+          //使用搜索引擎查询发音
+          box.goToSearchPronounce(event.target.innerText);
         }
-        event.preventDefault();
-        event.stopPropagation();
-        let audio_url = null;
-        if (event.target.nodeName === "TD") {
-          let aTag = event.target.querySelector("a");
-          if (aTag) {
-            audio_url = aTag.getAttribute("href");
-          }
-        }
-        if (event.target.nodeName === "IMG") {
-          let aTag = event.target.parentNode.parentNode;
+      }
+      event.preventDefault();
+      event.stopPropagation();
+      let audio_url = null;
+      if (event.target.nodeName === "TD") {
+        let aTag = event.target.querySelector("a");
+        if (aTag) {
           audio_url = aTag.getAttribute("href");
         }
-        if (event.target.nodeName === "G-EMOJI") {
-          let aTag = event.target.parentNode;
-          audio_url = aTag.getAttribute("href");
+      }
+      if (event.target.nodeName === "IMG") {
+        let aTag = event.target.parentNode.parentNode;
+        audio_url = aTag.getAttribute("href");
+      }
+      if (event.target.nodeName === "G-EMOJI") {
+        let aTag = event.target.parentNode;
+        audio_url = aTag.getAttribute("href");
+      }
+      if (audio_url) {
+        let desURL = new URL(audio_url);
+        //console.log(desURL.protocol);
+        if (desURL.protocol === "http:") {
+          //skip http
+          location.href = audio_url;
+        } else {
+          // console.log("audio_url:", audio_url);
+          audio_player.setAttribute("src", audio_url);
         }
-        if (audio_url) {
-          let desURL = new URL(audio_url);
-          //console.log(desURL.protocol);
-          if (desURL.protocol === "http:") {
-            //skip http
-            location.href = audio_url;
-          } else {
-            // console.log("audio_url:", audio_url);
-            audio_player.setAttribute("src", audio_url);
-          }
+      }
+    });
+
+    table_element.addEventListener("mouseover", (event) => {
+      let parentElement = event.target.parentElement;
+      if (parentElement && parentElement.nodeName === "TR") {
+        if (parentElement.firstElementChild === event.target) {
+          event.target.setAttribute("title", "点击我打开搜索引擎检索");
+          event.target.style.cursor = "pointer";
         }
-      });
-    document
-      .querySelector("#readme table tbody")
-      .addEventListener("mouseover", (event) => {
-        let parentElement = event.target.parentElement;
-        if (parentElement && parentElement.nodeName === "TR") {
-          if (parentElement.firstElementChild === event.target) {
-            event.target.setAttribute("title", "点击我打开搜索引擎检索");
-            event.target.style.cursor = "pointer";
-          }
-        }
-      });
-    let table = document.querySelector("#readme table");
+      }
+    });
+    let table = table_element;
     let parent = table.parentNode;
     let note = document.createElement("span");
     note.innerText = `⚪恢复扩展默认配置⚪`;
     note.setAttribute(
       "class",
-      "chinese-programmer-wrong-pronunciation-custom-note-reset"
+      "chinese-programmer-wrong-pronunciation-custom-note-reset",
     );
 
     note.addEventListener("click", (event) => {
